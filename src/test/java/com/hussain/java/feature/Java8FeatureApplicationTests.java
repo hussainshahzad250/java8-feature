ba68e1f1ce8411cf03eac05665e47aa7e9c14a3a
Shahzad Hussain <hussain.shahzad250@gmail.com>
2021-08-20 00:13:25 +0530

    Initial commit

---
+++ src/test/java/com/hussain/java/feature/Java8FeatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hussain.java.feature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8FeatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
